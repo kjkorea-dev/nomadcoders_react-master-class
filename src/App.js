e60fb35035fdfe612c5badcc0cf2217f6c10752d
kj-dev <agassi77.dev@gmail.com>
2022-11-15 17:52:00 +0900

    Attaching additional props : attr

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+  padding: 5px;
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Btn>Login</Btn>
-      <Btn as="a" href="/register">
-        Register
-      </Btn>
+    <Father>
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
