828e3782f180c27c3576d37a380bf1ccf7da8b1e
kj-dev <agassi77.dev@gmail.com>
2022-11-15 17:44:09 +0900

    Polymorphic prop : as

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
+
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Login</Btn>
+      <Btn as="a" href="/register">
+        Register
+      </Btn>
     </Father>
   );
 }
