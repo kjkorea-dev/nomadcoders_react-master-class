e9f3c84b63d76c93ca2308fe6e59bf11e69fbc4c
kj-dev <agassi77.dev@gmail.com>
2022-11-17 09:54:46 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,48 +10,13 @@ const Wrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 2s linear infinite;
-
-  ${Emoji}:hover {
-    font-size: 60px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😍</Emoji>
-      </Box>
-      <Emoji>😭</Emoji>
+      <Title>Hello Theme</Title>
     </Wrapper>
   );
 }
