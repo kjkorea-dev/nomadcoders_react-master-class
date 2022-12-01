86d76f9628806aff07c3eabdd8a4bbb15e90fc41
kj-dev <agassi77.dev@gmail.com>
2022-12-01 15:37:25 +0900

    Selectors : print to-dos for all categories

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import CreateTodo from "./CreateTodo";
-import { toDoState } from "../atoms";
+import { toDoSelector } from "../atoms";
 import ToDo from "./ToDo";
 
 const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
-
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateTodo />
+      <h2>To Do</h2>
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
