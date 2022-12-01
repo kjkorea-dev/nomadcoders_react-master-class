d7923cba95364a1f09d9a9134100280344b219f9
kj-dev <agassi77.dev@gmail.com>
2022-12-01 14:12:46 +0900

    Update categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
-import {IToDo} from "../atoms";
+import React from "react";
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
-const ToDo = ({text}:IToDo) => {
+const ToDo = ({ text, category, id }: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = ({
+    currentTarget: { name },
+  }: React.MouseEvent<HTMLButtonElement>) => {
+    setToDos((oldToDos) => {
+      const index = oldToDos.findIndex((toDo) => toDo.id === id);
+      return [
+        ...oldToDos.slice(0, index),
+        {
+          id,
+          text,
+          // to avoid type error
+          category: name as any,
+        },
+        ...oldToDos.slice(index + 1),
+      ];
+    });
+  };
   return (
-    <li>{text}</li>
+    <li>
+      <span>{text}</span>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
   );
 };
 
